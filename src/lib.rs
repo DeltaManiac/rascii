dbde69913860fc4ba03794fe57958771b919549f
DeltaManiac <maxpaynered@gmail.com>
2018-06-27 05:06:42 +0530

    added y scaling

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 // extern crate statrs;
 pub mod a {
 use std::cmp;
+use std::f64;
 // use statistical::{mean,standard_deviation};
 // use statrs::statistics::Variance;
     pub fn hello() {
@@ -21,14 +22,39 @@ use std::cmp;
         b
         }
 
-    fn scale_x_values(values:&[i32], max_width:i32)->&[i32]{
+    fn scale_x_values<'a,'b>( values:&'a[i32], max_width:i32)->&'a[f64]{
+       let mut scaled_value:Vec<f64> = Vec::new();
         if values.len() as i32 >max_width{
 
         }
-        values
-        
+        for i in values {
+            scaled_value.push(*i as f64)
+        }
+       &scaled_value[..]
     }
 
+    fn scale_y_value(values:&[f64],new_min:i32,new_max:i16,scale_from_old_zero:bool)->&[f64]{
+       let mut scaled_value:Vec<f64> = Vec::new();
+        let mut y_min_value = values.iter().fold(f64::INFINITY,|a,&b| a.min(b));
+        if scale_from_old_zero {
+            y_min_value = 0.0;
+        }
+        let mut y_max_value =   values.iter().fold(f64::INFINITY,|a,&b| a.min(b)); 
+        let new_min = 0;
+        let mut old_range = y_max_value - y_min_value;
+        if old_range == 0.0{
+            old_range = 1.0;
+        };
+        let new_range = new_max - new_min;
+        for i  in values {
+            let a:f64 = (i-y_min_value) as f64;
+            let b:f64= a * new_range as f64;
+            let c:f64 = b /old_range as f64;
+            let d:f64 = c + new_min as f64;
+            scaled_value.push(d);
+        }
+       &scaled_value[..]
+    }
     pub fn rasciigraph(values:Vec<i32>,height:Option<i16>,width:Option<i32>){
         let border_char = '*';
         let max_height = height.unwrap_or(180 );
@@ -38,8 +64,10 @@ use std::cmp;
         println!("MEAN {}", mean );
        let std_dev = standard_deviation(&values[..]);
         println!("std dev {:?}", std_dev );
-       let adjusted_value = scale_x_values(&values[..], max_width);
-       println!(" adjussted {:?}", adjusted_value )
+       let mut adjusted_value:&[f64] = scale_x_values(&values[..], max_width);
+       println!(" adjussted {:?}", adjusted_value );
+       adjusted_value = scale_y_value( &adjusted_value[..],0,max_height,false);
+       println!(" adjussted {:?}", adjusted_value );
     }
 
 }
